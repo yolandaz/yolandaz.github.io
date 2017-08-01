c0a6f10893e99894da3b89a55b721e509cd7af0e
Yolanda Zhou <yolandaz@mit.edu>
2017-08-01 15:58:51 -0700

    try json projects

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,15 @@
-//typing
-$(function(){
-    $(".type").typed({
-      strings: ["create.",  "design.", "code.", "solve.", "dream."],
-      typeSpeed: 50,
-      backSpeed: 50,
-      backDelay: 1000,
-      loop: true
-    });
+// typing
+$(function () {
+  $(".type").typed({
+    strings: ["create.", "design.", "code.", "solve.", "dream."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+  });
 });
 
-//particles
+// particles
 particlesJS('particles-js',
   {
     "particles": {
@@ -125,26 +125,47 @@ particlesJS('particles-js',
 );
 
 
-$( document ).ready(function() {
-  //click send button
-  $( "#contact-send" ).click(function() {
+// flexslider
+$(window).load(function () {
+  $('.flexslider').flexslider();
+});
+
+$(document).ready(function () {
+  // load projects from projects.json
+  $.getJSON("js/projects.json", function (data) {
+    console.log(data);
+    // var items = [];
+    // $.each(data, function (key, val) {
+    //   items.push("<li id='" + key + "'>" + val + "</li>");
+    // });
+
+    // $("<ul/>", {
+    //   "class": "my-new-list",
+    //   html: items.join("")
+    // }).appendTo("body");
+  });
+
+  // click send button
+  $("#contact-send").click(function () {
     var name = encodeURIComponent($("#contact-name").val());
     var subject = encodeURIComponent($("#contact-subject").val());
     var message = encodeURIComponent($("#contact-message").val());
     window.location.href = "mailto:me@yolandaz.com?subject=" + subject + "&body=" + message + "%0A%0A" + name;
   });
-  //smooth scrolling
-  $('#nav a').click(function(){
+
+  // smooth scrolling
+  $('#nav a').click(function () {
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
     return false;
   });
-  //view header when goes into view & scrollspy
+
+  // view header when goes into view & scrollspy
   var aboutSection = new Waypoint({
     element: $('#about')[0],
-    handler: function(direction) {
-      if (direction==="down"){
+    handler: function (direction) {
+      if (direction === "down") {
         $("#nav").removeClass("animated");
         $("#nav").addClass("visible");
         $("#nav-container a").removeClass("active");
@@ -155,127 +176,75 @@ $( document ).ready(function() {
     },
     offset: 50
   });
-  //scrollspy for the rest of them
+
+  // scrollspy for the rest of them
   var aboutSection = new Waypoint({
     element: $('#experience')[0],
-    handler: function(direction) {
-      if (direction==="down"){
+    handler: function (direction) {
+      if (direction === "down") {
         $("#nav-container a").removeClass("active");
-        $("#nav-"+this.element.id).addClass("active");
+        $("#nav-" + this.element.id).addClass("active");
       } else {
         $("#nav-container a").removeClass("active");
-        $("#nav-"+this.element.id).prev().addClass("active");
+        $("#nav-" + this.element.id).prev().addClass("active");
       }
     },
     offset: 50
   });
   var aboutSection = new Waypoint({
     element: $('#contact')[0],
-    handler: function(direction) {
-      if (direction==="down"){
+    handler: function (direction) {
+      if (direction === "down") {
         $("#nav-container a").removeClass("active");
-        $("#nav-"+this.element.id).addClass("active");
+        $("#nav-" + this.element.id).addClass("active");
       } else {
         $("#nav-container a").removeClass("active");
-        $("#nav-"+this.element.id).prev().addClass("active");
+        $("#nav-" + this.element.id).prev().addClass("active");
       }
     },
     offset: 50
   });
-  //project expanders
-  $('.project-item').click(function(){
-    //select correct expander
-    var expander = $("#"+$(this).attr("data-ex"));
 
-    //if clicked the open item, close it
+  // project expanders
+  $('.project-item').click(function () {
+    // select correct expander
+    var expander = $("#" + $(this).attr("data-ex"));
+
+    // if clicked the open item, close it
     if ($(this).hasClass("open")) {
       $(this).removeClass("open");
       expander.removeClass("open");
     } else {
-      //if not, close all
+      // if not, close all
       $(".project-item").removeClass("open");
       $(".project-expander").removeClass("open");
 
-      //move expander to right place
+      // move expander to right place
       var containerWidth = $("#project-container").width();
       var projectWidth = $(".project-item").width();
-      var itemsPerRow = Math.floor(containerWidth/projectWidth);
-      var projectIndex = $(this).index(".project-item")+1;
+      var itemsPerRow = Math.floor(containerWidth / projectWidth);
+      var projectIndex = $(this).index(".project-item") + 1;
       var numProjects = $('.project-item').length;
-      var insertIndex = Math.ceil(projectIndex/itemsPerRow)*itemsPerRow-1;
-      if (insertIndex >= numProjects){
+      var insertIndex = Math.ceil(projectIndex / itemsPerRow) * itemsPerRow - 1;
+      if (insertIndex >= numProjects) {
         insertIndex = numProjects - 1;
       }
-      var insertAfterItem = $(".project-item:eq("+insertIndex+")");
-      
-      expander.insertAfter(insertAfterItem); 
+      var insertAfterItem = $(".project-item:eq(" + insertIndex + ")");
 
-      //fixth width and positioning
-      expander.width($(window).width()-expander.css("padding-left").replace("px", "")-expander.css("padding-right").replace("px", ""));
-      expander.css("margin-left",-($(window).width()-$("#project-container").width())/2);
+      expander.insertAfter(insertAfterItem);
 
-      //display expander
+      // fixth width and positioning
+      expander.width($(window).width() - expander.css("padding-left").replace("px", "") - expander.css("padding-right").replace("px", ""));
+      expander.css("margin-left", -($(window).width() - $("#project-container").width()) / 2);
+
+      // display expander
       $(this).addClass("open");
       expander.addClass("open");
 
-      //jump bottom of page to bottom of expander
+      // jump bottom of page to bottom of expander
       $('html, body').animate({
         scrollTop: expander.offset().top + expander.outerHeight() - $(window).height()
-      }, 500); 
+      }, 500);
     }
   });
 });
-
-$(window).load(function() {
-  $('.flexslider').flexslider();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
